39b3a19075e0e330410e6b99bfcf359c35ff38f7
john Doe <jeffreyalmonte@Pursuits-MacBook-Pro-10.local>
2018-10-05 20:56:09 -0400

    first try

---
+++ github_1/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  github_1
+//
+//  Created by Jeffrey Almonte on 10/5/18.
+//  Copyright © 2018 Jeffrey Almonte. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+// github practice take 1
